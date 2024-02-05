ed35031c0a4991c2f0acd4a05497b1b5738f1a7e
saubuny <saubuny@gmail.com>
2024-02-04 22:30:53 -0500

    Drawing on the grid!

--- src/arch.rs
+++ src/arch.rs
@@ -1,19 +1,17 @@
-pub const WIDTH: usize = 64;
-pub const HEIGHT: usize = 32;
+use macroquad::{prelude::*, rand};
 
-const PIXEL_SIZE: f32 = 16.;
-const RESOLUTION: (f32, f32) = (PIXEL_SIZE * WIDTH as f32, PIXEL_SIZE * HEIGHT as f32);
+pub const PIXEL_SIZE: i32 = 16;
 
 #[derive(Debug)]
 #[allow(dead_code)]
 pub struct Chip8 {
-    ram: [u8; 4096],
-    display: [[u8; WIDTH]; HEIGHT],
-    pc: u8,
+    pub ram: [u8; 4096],
+    display: [[u8; 32]; 64],
+    pub pc: u8,
     index: u16,
     stack: Vec<u16>,
-    delay_timer: u8,
-    sound_timer: u8,
+    pub delay_timer: u8,
+    pub sound_timer: u8,
     v0: u8,
     v1: u8,
     v2: u8,
@@ -29,4 +27,72 @@ pub struct Chip8 {
     vd: u8,
     ve: u8,
     vf: u8,
+}
+
+impl Chip8 {
+    pub fn new() -> Self {
+        let mut display = [[0; 32]; 64];
+        for i in display.iter_mut().flat_map(|r| r.iter_mut()) {
+            *i = rand::gen_range(0, 2);
+        }
+
+        Chip8 {
+            ram: [0; 4096],
+            display,
+            pc: 0,
+            index: 0,
+            stack: vec![],
+            delay_timer: 0,
+            sound_timer: 0,
+            v0: 0,
+            v1: 0,
+            v2: 0,
+            v3: 0,
+            v5: 0,
+            v6: 0,
+            v7: 0,
+            v8: 0,
+            v9: 0,
+            va: 0,
+            vb: 0,
+            vc: 0,
+            vd: 0,
+            ve: 0,
+            vf: 0,
+        }
+    }
+
+    // Clear Screen
+    pub fn _00e0(&mut self) {}
+
+    // Jump
+    pub fn _1nnn(&mut self) {}
+
+    // Set Register VX
+    pub fn _6xnn(&mut self) {}
+
+    // Add to Register VX
+    pub fn _7xnn(&mut self) {}
+
+    // Set Index Register I
+    pub fn _annn(&mut self) {}
+
+    // Display
+    pub fn _dxyn(&mut self) {
+        clear_background(BLACK);
+
+        for (i, row) in self.display.iter_mut().enumerate() {
+            for (j, col) in row.iter_mut().enumerate() {
+                if *col == 1 {
+                    draw_rectangle(
+                        i as f32 * PIXEL_SIZE as f32,
+                        j as f32 * PIXEL_SIZE as f32,
+                        PIXEL_SIZE as f32,
+                        PIXEL_SIZE as f32,
+                        WHITE,
+                    )
+                }
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,46 @@
+use chip8::arch::*;
 use macroquad::prelude::*;
 
-#[macroquad::main("chip8")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "chip8".into(),
+        window_resizable: false,
+        window_width: PIXEL_SIZE * 64,
+        window_height: PIXEL_SIZE * 32,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
+    let mut chip8 = Chip8::new();
+
     loop {
-        clear_background(RED);
+        // Limit to 60fps, https://github.com/not-fl3/macroquad/issues/380
+        let minimum_frame_time = 1. / 60.;
+        let frame_time = get_frame_time();
+        if frame_time < minimum_frame_time {
+            let time_to_sleep = (minimum_frame_time - frame_time) * 1000.;
+            std::thread::sleep(std::time::Duration::from_millis(time_to_sleep as u64));
+        }
+
+        // Update timers
+        if chip8.delay_timer != 0 {
+            chip8.delay_timer -= 1;
+        }
+
+        if chip8.sound_timer != 0 {
+            chip8.sound_timer -= 1;
+        }
+
+        // ~8-12 instructions per frame
+        for _ in 0..8 {
+            // Fetch Instruction
+
+            // Decode and run
+        }
+
+        chip8._dxyn();
 
         next_frame().await;
     }
