8f08901a372cb44e11a9e9eb43e1907660f07733
saubuny <saubuny@gmail.com>
2024-02-17 02:26:56 -0500

    FINISH

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,5 +1,8 @@
 use chip8_core::emu::*;
-use sdl2::{event::Event, pixels::Color, rect::Rect, render::Canvas, video::Window};
+use sdl2::{
+    event::Event, keyboard::Keycode, pixels::Color, rect::Rect, render::Canvas, sys::KeyCode,
+    video::Window,
+};
 use std::{env, fs};
 
 const SCALE: u32 = 16;
@@ -51,8 +54,27 @@ fn main() {
     'gameloop: loop {
         for evt in event_pump.poll_iter() {
             match evt {
-                Event::Quit { .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'gameloop;
+                }
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = get_key(key) {
+                        chip8.keypress(k, true);
+                    }
+                }
+
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = get_key(key) {
+                        chip8.keypress(k, false);
+                    }
                 }
                 _ => (),
             }
@@ -61,8 +83,33 @@ fn main() {
         for _ in 0..TICKS_PER_FRAME {
             chip8.tick();
         }
-        chip8.tick_timers();
+        if chip8.tick_timers() {
+            // TODO: Play a beeping noise
+        };
+
         draw_screen(&chip8, &mut canvas);
+    }
+}
+
+fn get_key(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
     }
 }
 
